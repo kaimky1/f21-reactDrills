94a6c4602790ac3af3087f659f3e9d735566f509
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-05-27 18:57:14 -1000

    renaming files

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,30 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import patrick from './images/patrick.png';
 import './App.css';
 
 function App() {
-  const [username, setUsername] = useState('')
-
-  const[password, setPassword] = useState('')
-  
-  const changeHandler = (e) => {
-    setUsername(e.target.value)
-  }
-  
-  const changeHandler2 = (e) => {
-    setPassword(e.target.value)
-  }
-
-  const loginHandler = () => {
-    alert(`Username: ${username} Password: ${password}`)
-  }
-
   return (
     <div className="App">
-      <form onSubmit={loginHandler}>
-        <input type="text"onChange={changeHandler} name="username"></input>
-        <input type="text"onChange={changeHandler2} name="password"></input>
-        <button> Login </button>
-      </form>
+       
+        <img src={patrick} className="App-logo" alt="logo" />
+       
     </div>
   );
 }
