02cac19876776173b450b8541a23028f58859856
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-05-28 12:51:01 -1000

    app-4 changes

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('')
+
+  const[password, setPassword] = useState('')
+  
+  const changeHandler = (e) => {
+    setUsername(e.target.value)
+  }
+  
+  const changeHandler2 = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const loginHandler = () => {
+    alert(`Username: ${username} Password: ${password}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={loginHandler}>
+        <input type="text"onChange={changeHandler} name="username"></input>
+        <input type="text"onChange={changeHandler2} name="password"></input>
+        <button> Login </button>
+      </form>
     </div>
   );
 }
