15519624719a2597a63b24cac38de22dd48a18ac
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-05-28 20:36:56 -1000

    test

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import NewTask from './components/NewTask';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+const addTaskHandler = (task) => {
+  console.log(task)
+  setTasks(...tasks, task)
+  console.log(tasks)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTask addTask={addTaskHandler}></NewTask>
     </div>
   );
 }
---
+++ app-7/src/components/NewTask.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+
+
+const NewTask = (pr) => {
+
+    const [input, setInput] = useState('')
+  return (
+    <div>
+        <input type="text" onChange={(e) => setInput(e.target.value)}/>
+        <button onClick={() => setTasks([...tasks, input])}>Add Task</button>
+    </div>
+  )
+}
+
+export default NewTask
