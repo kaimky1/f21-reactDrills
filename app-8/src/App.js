8ff26e86a5ae07f758a0fbc37f9d48ba763982a1
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-05-29 18:51:53 -1000

    worked on API

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
+const baseURL = "https://swapi.dev/api/people/1"
+
 function App() {
+  const[post, setPost] = useState(null)
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      console.log(response.data)
+      setPost(response.data)
+    })
+  }, [])
+
+  if (!post) return null;
+  
+  const array = post.films.map((element, index) => {
+    return <h1>Film: {element}</h1>
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        test
-      </header>
+      <h1>{post.name}</h1>
+      {array}
     </div>
   );
 }
